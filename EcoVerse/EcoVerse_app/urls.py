5c0cfd2b12807a06464719eac9b4a87ca64b1996
antonie-riziki <antonriziki@gmail.com>
2025-10-25 14:28:25 +0300

    updated the system templates

--- EcoVerse/EcoVerse_app/urls.py
+++ EcoVerse/EcoVerse_app/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('registration', views.registration, name="registration"),
+    path('signin', views.signin, name="signin"),
+    path('dashboard', views.dashboard, name="dashboard"),
+    path('settings', views.settings, name="settings"),
+    path('analytics', views.analytics, name="analytics"),
+    path('impact', views.impact, name="impact"),
+    path('rewards', views.rewards, name="reward"),
 
 ]
--- EcoVerse/EcoVerse_app/views.py
+++ EcoVerse/EcoVerse_app/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def rewards(request):
+    return render(request, 'rewards.html')
+
+
+def impact(request):
+    return render(request, 'impact.html')
+
+
+def analytics(request):
+    return render(request, 'analytics.html')
