4ce909b15a68d62a90fc52895af6c0b0f489ae8e
antonie <antonriziki@gmail.com>
2026-01-24 21:00:26 +0300

    refactored the code and added the opik llm evalution endpoint

--- EcoVerse/EcoVerse_app/views.py
+++ EcoVerse/EcoVerse_app/views.py
@@ -22,6 +22,37 @@ africastalking.initialize(
 )
 
 
+
+"""
+
+Opik Configuration for Gemini AI Model
+
+"""
+
+
+
+# import os
+
+# import google.genai
+# from opik import configure 
+# from opik.integrations.genai import track_genai 
+
+# configure() 
+
+# # os.environ["GEMINI_API_KEY"] = "your-api-key-here"
+
+# client = google.genai.Client()
+# gemini_client = track_genai(client) 
+# response = gemini_client.models.generate_content(
+#     model="gemini-2.0-flash-001", contents="Write a haiku about AI engineering."
+# )
+# print(response.text)
+
+
+
+
+
+
 def get_gemini_response(prompt):
 
     model = genai.GenerativeModel("gemini-2.5-flash", 
